3fced468268630632c2c0778b5bd6c4560d2c5f2
Sublunar Space <info@sublunar.space>
2022-09-15 22:28:42 +0200

    added Coptic and Catholix Synaxarium

--- js/SL.Calendar.js
+++ js/SL.Calendar.js
@@ -168,7 +168,7 @@ SL.Calendar = (function() {
             }
         // checks if usecase is online, then uses server based calculation
         } else {
-            var url = "pmom.php?days="+days+"&ts="+ts+"&lat="+lat+"&lon="+lon;
+            var url = "https://almanac.sublunar.space/pmom.php?days="+days+"&ts="+ts+"&lat="+lat+"&lon="+lon;
             fetch(url)
             .then(function(response) {
                 return response.json();
